573f00963af968ae53991cbd8483c4afcc054eaf
Ryan Culbertson <ryan@spotify.com>
2014-09-28 13:38:44 -0400

    Use git ID in version

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
+GIT_SHORT_ID=`git rev-parse --short HEAD`
+
 # TODO make skip tests an env var
-mvn -e -DskipTests -Drevision=$GO_PIPELINE_COUNTER clean package
+mvn -e -DskipTests -Drevision=+$GO_PIPELINE_COUNTER.$GIT_SHORT_ID clean package
