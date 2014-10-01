ec3de78f47e5e16e5ea31a7074a6b0a9ee0755df
Ryan Culbertson <ryan@spotify.com>
2014-10-01 14:58:01 -0400

    remove Download string

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ GIT_SHORT_ID=`git rev-parse --short HEAD`
 
 #commenting out for testing
 #mvn -Drevision=+$GO_PIPELINE_COUNTER.$GIT_SHORT_ID nexus-staging:deploy-staged
-mvn -Drevision=+$GO_PIPELINE_COUNTER.$GIT_SHORT_ID org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep -v '\[' > target/version
+mvn -Drevision=+$GO_PIPELINE_COUNTER.$GIT_SHORT_ID org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep -v '\[' | grep -v Download > target/version
 
 #git push origin rculbertson/helios-go
 #TAGREF=refs/tags/$(git describe --abbrev=0 rculbertson/helios-go)
