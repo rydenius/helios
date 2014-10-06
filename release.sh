d3d18f2a72111a1b9467ee045b0786d0177b99bc
Ryan Culbertson <ryan@spotify.com>
2014-10-06 12:51:24 -0400

    Have release script copy debs into target dir so we can copy them easily

--- release.sh
+++ release.sh
@@ -11,6 +11,7 @@ GIT_SHORT_ID=`git rev-parse --short HEAD`
 #commenting out for testing
 #mvn -Drevision=+$GO_PIPELINE_COUNTER.$GIT_SHORT_ID nexus-staging:deploy-staged
 mvn -Drevision=+$GO_PIPELINE_COUNTER.$GIT_SHORT_ID org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep -v '\[' | grep -v Download > target/version
+find . -name *.deb | xargs -I FILE cp FILE target
 
 #git push origin rculbertson/helios-go
 #TAGREF=refs/tags/$(git describe --abbrev=0 rculbertson/helios-go)
