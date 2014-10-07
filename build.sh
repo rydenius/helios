3419bd436f3bb70983c69c560293791c6ad4bbcc
Ryan Culbertson <ryan@spotify.com>
2014-10-07 15:23:06 -0400

    add revisiontest

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 GIT_SHORT_ID=`git rev-parse --short HEAD`
 REVISION=+${GO_PIPELINE_COUNTER}.${GIT_SHORT_ID}
 
+echo XXXXXXXXXXXXXXX ${REVISION_TEST:-NOTSET}
+
 # TODO make skip tests an env var
 # The nexus-staging-plugin is configured to "deploy" jars into a local directory, not to sonatype.
 # The jars can be deployed to sonatype using release.sh.
