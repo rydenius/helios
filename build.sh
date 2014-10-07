23a27507ed5884b5efa29d948f95db24d0503653
Ryan Culbertson <ryan@spotify.com>
2014-10-06 20:43:03 -0400

    new script for putting artifacts in tarball

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ GIT_SHORT_ID=`git rev-parse --short HEAD`
 # The nexus-staging-plugin is configured to "deploy" jars into a local directory, not to sonatype.
 # The jars can be deployed to sonatype using release.sh.
 mvn -e -P sign-artifacts -DskipTests -Drevision=+$GO_PIPELINE_COUNTER.$GIT_SHORT_ID clean deploy
+
+./tar_artifacts.sh
---
+++ tar_artifacts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# Tar up the staged release and all the pom files. We will use these in subsequent build steps to
+# perform the actual release.
+tar -zcvf helios-staged-release.tar.gz `find . -name nexus-staging && find . -name pom.xml`
+
+# Copy all debian packages into ./debs, and put them into a tarball
+mkdir debs
+find . -name *.deb | xargs -I FILE cp FILE debs
+tar -C debs -zcf debs.tar.gz .
