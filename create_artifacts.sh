aa39f0e407803a670dfa90d52f02a8b3c8fc9afd
Ryan Culbertson <ryan@spotify.com>
2014-10-07 16:55:14 -0400

    Add revision file to tarball

--- create_artifacts.sh
+++ create_artifacts.sh
@@ -2,12 +2,15 @@
 
 REVISION=${1:-SNAPSHOT}
 
-# Use the maven-help-plugin to get the version number from the pom, and output it to a file
+# Write revision to file so other pipelines can use it later
+echo ${REVISION} > revision
+
+# Write the version number (hardcoded pom version + revision) so other pipelines can use it later
 mvn -Drevision=${REVISION} org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep -v '\[' | grep -v Download > version
 
-# Tar up the staged release and all the pom files. We will use these in subsequent build steps to
-# perform the actual release.
-tar -zcvf helios-staged-release.tar.gz release.sh version `find . -name nexus-staging && find . -name pom.xml`
+# Tar up the staged release, all the pom files, and some build files. We will use these in
+# subsequent build steps to perform the actual release.
+tar -zcvf helios-staged-release.tar.gz release.sh revision version `find . -name nexus-staging && find . -name pom.xml`
 
 # Copy all debian packages into ./debs, and put them into a tarball
 mkdir debs
