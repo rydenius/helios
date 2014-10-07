8138d41939b0c706d6f80eab708a2fe92c4f696c
Ryan Culbertson <ryan@spotify.com>
2014-10-07 13:45:40 -0400

    output file should be version not revision

--- create_artifacts.sh
+++ create_artifacts.sh
@@ -3,11 +3,11 @@
 REVISION=${1:-SNAPSHOT}
 
 # Use the maven-help-plugin to get the version number from the pom, and output it to a file
-mvn -Drevision=${REVISION} org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep -v '\[' | grep -v Download > revision
+mvn -Drevision=${REVISION} org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep -v '\[' | grep -v Download > version
 
 # Tar up the staged release and all the pom files. We will use these in subsequent build steps to
 # perform the actual release.
-tar -zcvf helios-staged-release.tar.gz revision `find . -name nexus-staging && find . -name pom.xml`
+tar -zcvf helios-staged-release.tar.gz version `find . -name nexus-staging && find . -name pom.xml`
 
 # Copy all debian packages into ./debs, and put them into a tarball
 mkdir debs
