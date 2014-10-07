7c24b32c80b38d84f479787cd5b88e6dcacfa6e5
Ryan Culbertson <ryan@spotify.com>
2014-10-07 13:22:50 -0400

    fixes

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
 GIT_SHORT_ID=`git rev-parse --short HEAD`
+REVISION=+$GO_PIPELINE_COUNTER.$GIT_SHORT_ID
 
 # TODO make skip tests an env var
 # The nexus-staging-plugin is configured to "deploy" jars into a local directory, not to sonatype.
 # The jars can be deployed to sonatype using release.sh.
-mvn -e -P sign-artifacts -DskipTests -Drevision=+$GO_PIPELINE_COUNTER.$GIT_SHORT_ID clean deploy
+mvn -e -P sign-artifacts -DskipTests -Drevision=$REVISION clean deploy
 
-./tar_artifacts.sh
+./create_artifacts.sh $REVISION
--- create_artifacts.sh
+++ create_artifacts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+REVISION=$1
+
+# Use the maven-help-plugin to get the version number from the pom, and output it to a file
+mvn -Drevision=$REVISION org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep -v '\[' | grep -v Download > version
+
 # Tar up the staged release and all the pom files. We will use these in subsequent build steps to
 # perform the actual release.
 tar -zcvf helios-staged-release.tar.gz `find . -name nexus-staging && find . -name pom.xml`
